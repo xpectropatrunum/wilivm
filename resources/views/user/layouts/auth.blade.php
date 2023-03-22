8aa1b26d7cdac83decbb722687da02a5e0d5b25b
Amir <amirtaja@yahoo.com>
2023-03-22 13:16:30 +0430

    f

--- resources/views/user/layouts/auth.blade.php
+++ resources/views/user/layouts/auth.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex" />
+
     @yield('meta_tags')
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
--- resources/views/user/layouts/error.blade.php
+++ resources/views/user/layouts/error.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex" />
+
     <title>{{ title }} - {{ web_title }}</title>
     <link rel="stylesheet" href="{{asset("assets/css/main/app.css")}}">
     <link rel="stylesheet" href="{{asset("assets/css/pages/error.css")}}">
