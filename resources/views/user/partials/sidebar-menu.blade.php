61f45625786a93ec05b12feb0fb0595b7dcdec0e
Your Name <amirtaja@yahoo.com>
2023-04-09 23:53:59 +0430

    f

--- resources/views/user/partials/sidebar-menu.blade.php
+++ resources/views/user/partials/sidebar-menu.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-header position-relative">
         <div class="d-flex justify-content-between align-items-center">
             <div class="logo" >
-                <a href="/"><img src="{{ asset('assets/images/logo/wili-white.svg') }}" style="height: 75px;" alt="Wilivm" srcset=""></a>
+                <a href="/"><img src="{{ asset('assets/images/logo/wili-blue.svg') }}" style="height: 75px;" alt="Wilivm" srcset=""></a>
             </div>
             <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
