c7d0643445417594569a2798fdaa615fb0a1e56b
Your Name <amirtaja@yahoo.com>
2023-06-03 22:52:42 +0330

    r

--- resources/views/admin/pages/users/edit.blade.php
+++ resources/views/admin/pages/users/edit.blade.php
@@ -30,21 +30,21 @@
                     <input type="hidden" name="source" value="{{ config('global.user_source') }}">
                     <div class="card-body">
                         <div class="row">
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-3">
                                 <label>First name</label>
                                 <input type="text" value="{{ old('first_name', $user->first_name) }}" name="first_name" class="form-control @error('first_name') is-invalid @enderror" required>
                             </div>
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-3">
                                 <label>Last name</label>
                                 <input type="text" value="{{ old('last_name', $user->last_name) }}" name="last_name" class="form-control @error('last_name') is-invalid @enderror" required>
                             </div>
                     
                         
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-3">
                                 <label>Email</label>
                                 <input type="email" value="{{ old('email', $user->email) }}" name="email" class="form-control @error('email') is-invalid @enderror" required>
                             </div>
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-3">
                                 <label>Phone</label>
                                 <input type="phone" value="{{ old('phone', $user->phone) }}" name="phone" class="form-control @error('phone') is-invalid @enderror" required>
                             </div>
@@ -59,24 +59,24 @@
                                     @endforeach
                                 </select>
                             </div>
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-3">
                                 <label>State</label>
                                 <input type="state" value="{{ old('state', $user->state) }}" name="state" class="form-control @error('state') is-invalid @enderror" required>
                             </div>
 
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-3">
                                 <label>City</label>
                                 <input type="city" value="{{ old('city', $user->city) }}" name="city" class="form-control @error('city') is-invalid @enderror" required>
                             </div>
-                            <div class="form-group col-lg-8">
+                            <div class="form-group col-lg-9">
                                 <label>Address</label>
                                 <input type="address" value="{{ old('address', $user->address) }}" name="address" class="form-control @error('address') is-invalid @enderror" required>
                             </div>
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-3">
                                 <label>Password</label>
                                 <input type="text" value="{{ old('password') }}" name="password" class="form-control @error('password') is-invalid @enderror">
                             </div>
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-3">
                                 <label>Repeat password</label>
                                 <input type="text" value="{{ old('password_confirm') }}" name="password_confirm" class="form-control @error('password_confirm') is-invalid @enderror">
                             </div>
