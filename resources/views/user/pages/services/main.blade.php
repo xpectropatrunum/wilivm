b4ad9b1b2ca2e3cc99e128d8066514ce65b5a0fb
Your Name <amirtaja@yahoo.com>
2023-06-28 21:07:19 +0330

    r

--- resources/views/user/pages/services/main.blade.php
+++ resources/views/user/pages/services/main.blade.php
@@ -67,18 +67,21 @@
                                         @elseif ($item->status == 5)
                                             <span
                                                 class="badge bg-danger">{{ App\Enums\EServiceType::getKey($item->status) }}</span>
+                                        @elseif ($item->status == 6)
+                                            <span
+                                                class="badge bg-danger">{{ App\Enums\EServiceType::getKey($item->status) }}</span>
+                                        @elseif ($item->status == 7)
+                                            <span
+                                                class="badge bg-danger">{{ App\Enums\EServiceType::getKey($item->status) }}</span>
                                         @endif
                                     </td>
                                     <td>{{ MyHelper::due($item->order) }}</td>
                                     <td>
 
-                                        @if ($item->status != 5 && $item->status != 4)
-                                            <span
-                                                class="badge bg-success">{{ App\Enums\EServiceType::getKey($item->status) }}</span>
-
+                                        @if ($item->status == 2)
                                             <a href="{{ route('panel.services.show', $item->id) }}"
                                                 class="btn btn-outline-primary">
-                                                Manage Service</a>  </span>
+                                                Manage Service</a> </span>
                                         @endif
                                     </td>
                                 </tr>
