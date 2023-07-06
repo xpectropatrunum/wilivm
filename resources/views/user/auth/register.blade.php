bae643cb2a95630d54867fbf38045f349f04dfb7
Your Name <amirtaja@yahoo.com>
2023-07-06 21:14:52 +0330

    r

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -272,20 +272,23 @@
         colorScore3: '#aaa',
         colorScore4: 'limegreen'
       });
-      if(myPassMeter.getScore() == 3){
-        $("#kt_login_signin_submit").removeAttr("disabled")
-    }else{
-        $("#kt_login_signin_submit").attr("disabled", "disabled")
-    }
+      setTimeout(()=>{
+        if(myPassMeter.getScore() == 4){
+            $("#kt_login_signin_submit").removeAttr("disabled")
+        }else{
+            $("#kt_login_signin_submit").attr("disabled", "disabled")
+        }
+      }, 2000)
+   
       $("[name=password]").on("input", function(){
-        if(myPassMeter.getScore() >= 3){
+        if(myPassMeter.getScore() >= 4){
             $("#kt_login_signin_submit").removeAttr("disabled")
         }else{
             $("#kt_login_signin_submit").attr("disabled", "disabled")
         }
       })
       $("[name=password]").on("change", function(){
-        if(myPassMeter.getScore() >= 3){
+        if(myPassMeter.getScore() >= 4){
             $("#kt_login_signin_submit").removeAttr("disabled")
         }else{
             $("#kt_login_signin_submit").attr("disabled", "disabled")
